b2b175ced727c4921d01e92dcba7fad5008cf015
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2014-12-12 01:29:54 -0800

    some more mods

---
+++ script/app_specifics.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Original script on https://github.com/orendon/vagrant-rails
+
+# load rbenv and shims
+export RBENV_ROOT="${HOME}/.rbenv"
+export PATH="${RBENV_ROOT}/bin:${PATH}"
+export PATH="${RBENV_ROOT}/shims:${PATH}"
+
+git clone https://github.com/Pick6Solutions/pick6.git
+cd pick6/rails
+bundle install
+rbenv rehash
+# sudo sudo -u postgres psql -1 -c "CREATE USER pgadmin WITH PASSWORD 'secret';"
+# sudo sudo -u postgres psql -1 -c "ALTER USER pgadmin WITH SUPERUSER;"
+# cp -R config/database.sample.yml config/database.yml
+rake db:create
+rake db:migrate
+rake db:seed
