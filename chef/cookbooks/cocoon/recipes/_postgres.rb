61e1aae4f538a8cac993108ddaa257312e834c85
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2014-12-13 18:50:14 -0800

    .

--- chef/cookbooks/cocoon/recipes/_postgres.rb
+++ chef/cookbooks/cocoon/recipes/_postgres.rb
@@ -24,7 +24,7 @@ execute 'createuser' do
   user 'postgres'
   command 'createuser vagrant'
   not_if guard, user: 'postgres'
-  psql << EOF
-  	ALTER ROLE vagrant CREATEDB;
+  psql <<-EOF
+  	ALTER ROLE vagrant CREATEDB
   EOF
 end
